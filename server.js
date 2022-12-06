33d9db80c7dec1644255610be6180d8315405002
Brendan Doherty <brendandoherty@Brendans-MBP.lan>
2022-12-06 18:41:24 -0500

    first commit

--- server.js
+++ server.js
@@ -5,32 +5,31 @@ const morgan = require("morgan")
 const methodOverride = require("method-override")
 const mongoose = require("mongoose")
 
-// create express app
-
+// create express application
 const app = express()
 
-//establish mongo connection
+// establish mongo connection
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.DATABASE_URL)
 
 // mongoose connection events
 mongoose.connection
-.on("open", [] => console.log["Connected to mongo"] )
-.on("close", [] => console.log["Disconnected to Mongo"])
-.on("error", [error] => console.log[error])
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected from Mongo"))
+.on("error", (error) => console.log(error))
 
 // register middleware
-app.use(morgan["dev"])
-app.use("/static", express.static[public])
-app.use(express.urlencoded[{extended: true}])
-app.use(methodOverride["_method"])
+app.use(morgan("dev"))
+app.use(express.static("public"))
+app.use("/static", express.static ("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
-
-// Routes & Router
-app.get("/", [req, res] => {
-    res.send["<h1>Server is working</h1>"]  
+// routes and routers
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working</h1>")
 })
 
-//start the server(listener)
-
+// start the server (listener)
 const PORT = process.env.PORT || 3000
-app.listen(PORT, [] => console.log[`Listening on port${PORT}`])
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
